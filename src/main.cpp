0edc09e22a667529b6d4c522e7fd43ee8a8e61b3
Nikodem Klimała <nikodemklimala@interia.eu>
2022-06-29 16:48:13 +0200

    Final version

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
-
+#include "map.hpp"
 #include "mower.hpp"
-
-int main(){
-    std::cout << "Hello world!" << std::endl;
-
-    Lawn my_lawn("basic_map.txt");
-    my_lawn.display_map();
-    my_lawn.move_mower(up);
-    return 0;
-}
-
-/*
 
 int main() {
     Mow mower;
@@ -19,6 +8,17 @@ int main() {
     agent.learn(mower, 1000);
     agent.print();
     agent.play(mower);
+    return 0;
+}
+
+
+/*
+int main(){
+    std::cout << "Hello world!" << std::endl;
+
+    Lawn my_lawn("basic_map.txt");
+    my_lawn.display_map();
+    my_lawn.move_mower(up);
     return 0;
 }
 */
